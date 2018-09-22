b46bcb648026e6613f297d19b98a69767e731d5a
David Arnold <dja+djaboxx@sonic.net>
2018-09-21 20:05:11 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,12 +4,21 @@ resource "null_resource" "profiler" {
   }
   
   provisioner "local-exec" {
-    command = "sudo apt-get install -y facter"
+    command = "sudo apt-get install -y facter net-tools"
   }
   
   provisioner "local-exec" {
     command = "sudo facter"
   }
+  
+  provisioner "local-exec" {
+    command = "sudo ps auxw"
+  }
+  
+  provisioner "local-exec" {
+    command = "sudo netstat -natp"
+  }
+  
 }
 
 
