ee55810c857bb8af2597dcdf2a3276ba44a59b46
David Arnold <dja+djaboxx@sonic.net>
2018-09-21 20:07:00 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+
+data "external" "random_bytes" {
+  program = ["python", "${path.module}/scripts/random_output.py"]
+}
+
 resource "null_resource" "profiler" {
   provisioner "local-exec" {
     command = "sudo apt-get update"
