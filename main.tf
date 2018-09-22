f85721839305b7425249d4b1c1aaffde1880dcb9
David Arnold <dja+djaboxx@sonic.net>
2018-09-21 20:00:23 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "null_resource" "profiler" {
   provisioner "local-exec" {
-    command = "sudo apt-get install facter"
+    command = "sudo apt-get install -y facter"
   }
   
   provisioner "local-exec" {
-    command = "facter"
+    command = "sudo facter"
   }
 }
 
